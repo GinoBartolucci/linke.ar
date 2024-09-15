f3598ff0f756b6dcb1587f2a41618516f6cd6215
gino bartolucci <ginobartolucci00@gmail.com>
2024-09-14 21:26:33 -0300

    endpoint

--- app/[shortUrl]/page.tsx
+++ app/[shortUrl]/page.tsx
@@ -3,19 +3,13 @@ import { redirect } from 'next/navigation'
 export default async function ShortUrl({ params }: { params: { shortUrl: String } }) {
   const shortUrl = params.shortUrl;
   //const response = await fetch("http://localhost:3000/api/url/" + shortUrl, {
-  try {
-
-    const response = await fetch("https://linke-ar.ginobartolucci.com.ar/api/url/" + shortUrl, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    const data = await response.json()
-    console.log(data.originalUrl)
-    redirect("https://" + data.originalUrl)
-  }
-  catch (e: any) {
-    console.log(e.message)
-  }
+  const response = await fetch("https://linke-ar.ginobartolucci.com.ar/api/url/" + shortUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  const data = await response.json()
+  console.log(data.originalUrl)
+  redirect("https://" + data.originalUrl)
 }
